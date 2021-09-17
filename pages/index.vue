cbd004dbfce643bfb796d808acecbd4f3d359bf3
thanapongsj <thanapongsj1996@gmail.com>
2021-09-18 02:28:41 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Tutorial :msg="msg" />
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const data = await $axios.$get('https://arcane-chamber-55473.herokuapp.com/')
+    return { msg: data.message ? data.message : '' }
+  },
+  data() {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
