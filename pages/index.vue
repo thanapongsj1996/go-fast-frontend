810b2fec3611fa7c8a89558060a3ef4a531fe8bf
thanapongsj <thanapongsj1996@gmail.com>
2021-10-02 12:40:03 +0700

    Remove log

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,6 @@ export default {
   components: { Homepage },
   async mounted() {
     const response = await this.$axios.$get(`https://polar-tundra-74605.herokuapp.com/api/v1/jobs/customer-requests`)
-    console.log('response : ', response)
     if (response.status) {
       this.jobs = response.data ? response.data : []
     }
