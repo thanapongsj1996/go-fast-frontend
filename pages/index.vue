037100e5df21f671f001e25e591a0e43beda655b
thanapongsj <thanapongsj1996@gmail.com>
2021-10-04 16:58:49 +0700

    Add vercel.json for ssr

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,20 @@
 import Homepage from '~/components/Homepage.vue'
 export default {
   components: { Homepage },
-  async mounted() {
-    const response = await this.$axios.$get(`https://polar-tundra-74605.herokuapp.com/api/v1/jobs/customer-requests`)
+  async asyncData({ $axios }) {
+    const response = await $axios.$get(`https://polar-tundra-74605.herokuapp.com/api/v1/jobs/customer-requests`)
     if (response.status) {
-      this.jobs = response.data ? response.data : []
+      return {
+        jobs: response.data ? response.data : []
+      }
     }
+    return { jobs: [] }
+  },
+  async mounted() {
+    // const response = await this.$axios.$get(`https://polar-tundra-74605.herokuapp.com/api/v1/jobs/customer-requests`)
+    // if (response.status) {
+    //   this.jobs = response.data ? response.data : []
+    // }
   },
   data() {
     return {
