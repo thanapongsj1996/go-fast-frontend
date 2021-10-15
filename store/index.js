7bd72d1d1e32309fa4405390580dcbd43732b452
thanapongsj <thanapongsj1996@gmail.com>
2021-10-15 21:13:42 +0700

    Remove store

--- store/index.js
+++ store/index.js
@@ -1,18 +1,18 @@
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
 
 export const actions = {
-    async nuxtClientInit({ dispatch }, context) {
-        const user = await Cookies.get('user_profile')
-        const token = await Cookies.get('token')
+    // async nuxtClientInit({ dispatch }, context) {
+    //     const user = await Cookies.get('user_profile')
+    //     const token = await Cookies.get('token')
 
-        if (user === '' || token === '') {
-            dispatch('user/updateAuthStatus', false)
-            return
-        }
+    //     if (user === '' || token === '') {
+    //         dispatch('user/updateAuthStatus', false)
+    //         return
+    //     }
 
-        if (user !== '' && token !== '') {
-            // dispatch('user/updateAuthStatus', true)
-            dispatch('user/updateUser', JSON.parse(user))
-        }
-    }
+    //     if (user !== '' && token !== '') {
+    //         // dispatch('user/updateAuthStatus', true)
+    //         dispatch('user/updateUser', JSON.parse(user))
+    //     }
+    // }
 }
