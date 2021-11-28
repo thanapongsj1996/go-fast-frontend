f205a18ebadf6df5ff52419a8dd5101d5ab7e10d
thanapongsj <thanapongsj1996@gmail.com>
2021-11-28 18:30:20 +0700

    feature: add pagination

--- components/templates/DriversPage.vue
+++ components/templates/DriversPage.vue
@@ -1,13 +1,23 @@
 <template>
   <b-container>
     <h2 class="mt-4 mb-3 mt-md-5 mb-md-3 prompt-font">พาร์ทเนอร์ของเรา</h2>
-    <div v-if="drivers.length" class="row">
+    <div v-if="driverArr.length" class="row">
       <div
-        v-for="driver in drivers"
+        v-for="driver in driverArr"
         :key="driver.uuid"
         class="col-12 col-lg-6 my-2"
       >
         <DriverCard :driver="driver" />
+      </div>
+      <div class="mt-3 w-100">
+        <b-pagination
+          @change="changePage"
+          v-model="currentPage"
+          pills
+          :total-rows="rows"
+          :per-page="parPage"
+          align="center"
+        ></b-pagination>
       </div>
     </div>
     <div v-else class="text-center mt-5">ไม่มีพาร์ทเนอร์ส่งของในขณะนี้</div>
@@ -21,6 +31,23 @@ export default {
       type: Array,
       default: () => [],
     },
+    paging: {
+      type: Object,
+      default: () => {},
+    },
   },
+  data() {
+    return {
+      driverArr: this.drivers,
+      rows: this.paging ? this.paging.count : 0,
+      parPage: this.paging ? this.paging.limit : 14,
+      currentPage: this.paging ? this.paging.page : 1,
+    }
+  },
+  methods: {
+    changePage(page) {
+      this.$emit('getDriversByPage', page)
+    }
+  }
 }
 </script>
--- pages/drivers/index.vue
+++ pages/drivers/index.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container>
-    <DriversPage v-if="!pageLoading" :drivers="drivers" />
+    <DriversPage v-if="!pageLoading" :drivers="drivers" :paging="paging" @getDriversByPage="getDriversByPage" />
     <PageLoading v-else-if="pageLoading" />
   </b-container>
 </template>
@@ -20,19 +20,29 @@ export default {
     }
   },
   async created() {
-    this.pageLoading = true
-    const response = await this.$axios.$get(
-      `${process.env.API_ENDPOINT}/driver-jobs/provided-jobs`
-    )
-    if (response.status) {
-      this.drivers = response.data ? response.data : []
-    }
-    this.pageLoading = false
+    await this.getDrivers()
   },
   data() {
     return {
       pageLoading: false,
       drivers: [],
+      paging: {},
+    }
+  },
+  methods: {
+    async getDrivers(page = 1, limit = 14) {
+      this.pageLoading = true
+      const response = await this.$axios.$get(
+        `${process.env.API_ENDPOINT}/driver-jobs/provided-jobs?limit=${limit}&page=${page}`
+      )
+      if (response.status) {
+        this.drivers = response.data ? response.data : []
+        this.paging = response.paging ? response.paging : {}
+      }
+      this.pageLoading = false
+    },
+    async getDriversByPage(page) {
+      await this.getDrivers(page)
     }
   },
 }
